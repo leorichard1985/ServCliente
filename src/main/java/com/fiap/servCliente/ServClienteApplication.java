5273ecdf5839ce622cbbfc3b7d29dff6b0fba93b
Leandro Conceição <leandro.vs2010@hotmail.com>
2024-07-15 21:24:43 -0300

    estrutura projeto

---
+++ src/main/java/com/fiap/servCliente/ServClienteApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.servCliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServClienteApplication.class, args);
+	}
+
+}
